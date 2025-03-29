c23dcd794c832f174facf4e67da92b4bebe6c675
Mateo Cante <m.canteg111@gmail.com>
2025-03-29 12:09:35 -0500

    Cambios Home

--- armonia/urls.py
+++ armonia/urls.py
@@ -1,4 +1,4 @@
-"""monitoring URL Configuration
+"""armonia URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
